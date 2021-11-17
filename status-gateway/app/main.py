09e2a673c04f2ed6453ed3f0c8651b5f2e86d541
Arjan <33868752+ajvreugdenhil@users.noreply.github.com>
2021-11-17 12:27:00 +0100

    correct colors

--- status-gateway/app/main.py
+++ status-gateway/app/main.py
@@ -6,6 +6,7 @@ import printermodule
 import printer
 
 temperature_margin = 5
+max_cold_temperature = 35
 
 stopflag = False
 
@@ -38,11 +39,16 @@ while not stopflag:
     for color in lampstates:
         m.send_message(color + "-" + lampstates[color])
     time.sleep(1)
-    # Define status parameters
+    # TODO: resend count
+    # TODO: vibrations
+    
     status = p.get_printer_endpoint("/api/printer")
     if status is not None:
-        is_tool0_warming = status["temperature"]["tool0"]["actual"] < status["temperature"]["tool0"]["target"] - temperature_margin
-        is_bed_warming = status["temperature"]["bed"]["actual"] < status["temperature"]["bed"]["target"] - temperature_margin
+        is_reachable = True
+        is_tool0_hot = status["temperature"]["tool0"]["actual"] > status["temperature"]["tool0"]["target"] - temperature_margin
+        is_bed_hot = status["temperature"]["bed"]["actual"] > status["temperature"]["bed"]["target"] - temperature_margin
+        is_tool0_warm = status["temperature"]["tool0"]["actual"] > max_cold_temperature
+        is_bed_warm = status["temperature"]["bed"]["actual"] > max_cold_temperature
         is_error = status["state"]["flags"]["error"]
         is_closed_or_error = status["state"]["flags"]["closedOrError"]
         is_printing = status["state"]["flags"]["printing"]
@@ -51,11 +57,35 @@ while not stopflag:
         is_paused = status["state"]["flags"]["paused"]
         is_cancelling = status["state"]["flags"]["cancelling"]
         is_printing = status["state"]["flags"]["printing"]
+
+        if is_error or is_closed_or_error:
+            lampstates["red"] = "on"
+        else:
+            lampstates["red"] = "off"
+
+        if not is_ready:
+            lampstates["orange"] = "on"
+        else:
+            lampstates["orange"] = "off"
+
+        if is_printing:
+            if not is_bed_hot or not is_tool0_hot:
+                lampstates["green"] = "blink"
+            else:
+                lampstates["green"] = "on"
+        else:
+            lampstates["green"] = "off"
+
+        if is_cancelling or is_pausing:
+            lampstates["white"] = "blink"
+        elif is_paused or not is_printing:
+            lampstates["white"] = "on"
+        else:
+            lampstates["white"] = "off"
+
     else:
         is_reachable = False
-
-    # TODO: resend count
-    # TODO: vibrations
+        lampstates["orange"] = "blink"
     
 
 m.send_message("red-off")
